49cba27b08729085d1ee166cb8f1b4a383d16c6a
Snehal Sanjay Mankar <68723397+mankarsnehal@users.noreply.github.com>
2023-07-17 22:04:40 +0530

    Initial Commit

---
+++ CollectionsFramework.java
@@ -0,0 +1,13 @@
+// Program number 5
+
+public class CollectionsFramework {
+  // Choosing right collection depends on:
+  // 1. Ordering - Sometimes order imp
+  // 2. Duplicates - Sometimes allowing duplicates in the obj is imp
+  // 3. Speed - How fast the coll is in performing specific operations
+  // Some are fast in adding, some are in removing records.
+  // 4. Memory - Some types take more memory than the others.
+  public static void main(String[] args) {
+
+  }
+}
